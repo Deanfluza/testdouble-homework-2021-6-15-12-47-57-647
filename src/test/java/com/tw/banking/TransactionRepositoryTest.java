b4a882a08a1b0911da4a986dfe994584110aa1e2
dean <deanms@qq.com>
2021-06-16 21:41:00 +0800

    Add Test of TransactionRepository's allTransactions func

--- src/test/java/com/tw/banking/TransactionRepositoryTest.java
+++ src/test/java/com/tw/banking/TransactionRepositoryTest.java
@@ -3,6 +3,7 @@ package com.tw.banking;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -40,4 +41,21 @@ class TransactionRepositoryTest {
         assertEquals(-100, transactionRepository.transactions.get(0).amount());
     }
 
+    @Test
+    void should_return_correct_trasaction_list_when_allTransactions_execute() {
+        //given
+        int amount = 100;
+        Clock clock = mock(Clock.class);
+        TransactionRepository transactionRepository = new TransactionRepository(clock);
+
+        //when
+        transactionRepository.addWithdraw(amount);
+        transactionRepository.addDeposit(amount);
+        List<Transaction> transactions = transactionRepository.allTransactions();
+
+        //then
+        assertEquals(2, transactions.size());
+        assertEquals(-100, transactions.get(0).amount());
+        assertEquals(100, transactions.get(1).amount());
+    }
 }
