d53235d3a2aaeaee787ae1ba900126c4e562783f
dean <deanms@qq.com>
2021-06-16 21:00:03 +0800

    Add Test of Transaction's compareTo that compare transaction time is after my transaction

--- src/test/java/com/tw/banking/TransactionTest.java
+++ src/test/java/com/tw/banking/TransactionTest.java
@@ -30,14 +30,30 @@ class TransactionTest {
         //given
         int amount = anyInt();
         String now = "16/06/2021";
-        String past = "16/06/2021";
+        String theSameDate = "16/06/2021";
         Transaction transactionForNow = new Transaction(now, amount);
-        Transaction transactionForPast = new Transaction(past, amount);
+        Transaction transactionForTheSameDate = new Transaction(theSameDate, amount);
 
         //when
-        int result = transactionForNow.compareTo(transactionForPast);
+        int result = transactionForNow.compareTo(transactionForTheSameDate);
 
         //then
         assertEquals(1, result);
+    }
+
+    @Test
+    void should_return_minus_1_when_compare_transaction_date_is_same_as_my_transaction() {
+        //given
+        int amount = anyInt();
+        String now = "16/06/2021";
+        String future = "17/06/2021";
+        Transaction transactionForNow = new Transaction(now, amount);
+        Transaction transactionForFuture = new Transaction(future, amount);
+
+        //when
+        int result = transactionForNow.compareTo(transactionForFuture);
+
+        //then
+        assertEquals(-1, result);
     }
 }
